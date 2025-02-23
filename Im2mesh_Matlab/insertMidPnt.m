ced3dce297304d696a4237850b15a5eece072435
Jiexian Ma <mjx0799@gmail.com>
2025-02-23 00:28:48 -0500

    make it work for polyline

--- Im2mesh_Matlab/insertMidPnt.m
+++ Im2mesh_Matlab/insertMidPnt.m
@@ -1,21 +1,18 @@
 function xyNew = insertMidPnt(xy)
-% insertMidPnt: inserts midpoints between vertices of a closed polygon.
+% insertMidPnt: inserts midpoints between vertices of a polyline
 %
-% takes the coordinates x, y of a closed 2D polygon (where x(end) = x(1)
-% and y(end) = y(1)), and returns new coordinates xNew, yNew. The new
-% coordinates will have midpoints inserted on each edge
+% takes the coordinates x, y of a 2D polyline, and returns new coordinates 
+% xNew, yNew. The new coordinates will have midpoints inserted on each edge
 %
 
     x = xy(:,1);
     y = xy(:,2);
 
-    % Check if input polygon is closed
+    % number of vertex
     n = length(x);
-    if x(n) ~= x(1) || y(n) ~= y(1)
-        error('The polygon must be closed: x(end) = x(1), y(end) = y(1).');
-    end
 
-    % Number of edges is (n-1). We add two vertices per edge + final closure.
+    % Number of edges is (n-1). 
+    % We add two vertices per edge + final closure.
     m = 2*(n - 1) + 1;
     xNew = zeros(m, 1);
     yNew = zeros(m, 1);
@@ -31,9 +28,9 @@ function xyNew = insertMidPnt(xy)
         yNew(2*i) = 0.5 * (y(i) + y(i+1));
     end
 
-    % Close the polygon explicitly
-    xNew(end) = xNew(1);
-    yNew(end) = yNew(1);
+    % last vertex
+    xNew(end) = x(end);
+    yNew(end) = y(end);
 
     xyNew = [xNew, yNew];
 
