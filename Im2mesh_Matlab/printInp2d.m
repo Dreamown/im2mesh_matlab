f5cd250044bba67ecf0e53a6ff8165d3d4a4600f
Jason <mjx0799@gmail.com>
2025-03-16 13:47:57 -0400

    Update printInp2d.m

--- Im2mesh_Matlab/printInp2d.m
+++ Im2mesh_Matlab/printInp2d.m
@@ -1,17 +1,58 @@
-function printInp2d( vert, tria, tnum, ele_type, precision_nodecoor, path_file_name )
+function printInp2d( vert, ele, tnum, ele_type, precision_nodecoor, path_file_name )
 % printInp2d: write 2d finite element mesh (nodes and elements) into inp 
-%           file ('test.inp'). Test in software Abaqus. 
-%           One part with multiple sections. 
-%           Each phase corresponds to one section in Abaqus.
+%           file. Test in software Abaqus. 
+%           The exported inp file will have a model with one part, which 
+%           contains multiple sections. Each section corresponds to one 
+%           material phase in the mesh.
+%           Use functions: getNodeEle
+%
 %           Works for linear and quadratic element.
 %           Works for triangular and quadrilateral element.
 %
 % usage:
-%   printInp_multiSect( nodecoor_list, ele_cell, ele_type, precision_nodecoor );
-%   % OR
-%   path_file_name = 'C:\Downloads\aaa.bdf';
-%   printInp_multiSect( nodecoor_list, ele_cell, ele_type, precision_nodecoor, path_file_name );
+%   printInp2d( vert, ele );
+%   printInp2d( vert, ele, [], [], [], path_file_name );
+%   printInp2d( vert, ele, tnum );
+%   printInp2d( vert, ele, tnum, [], precision_nodecoor );
+%   printInp2d( vert, ele, tnum, ele_type, precision_nodecoor );
+%   printInp2d( vert, ele, tnum, ele_type, precision_nodecoor, path_file_name );
 %
+% input:
+%   ele_type, precision_nodecoor, path_file_name are optional.
+%
+%   vert: Mesh nodes. It’s a Nn-by-2 matrix, where 
+%         Nn is the number of nodes in the mesh. Each row of vert 
+%         contains the x, y coordinates for that mesh node.
+%     
+%   ele: Mesh elements. 
+%        For linear triangular elements, it s a Ne-by-3 matrix. 
+%        For linear quadrilateral elements, it s a Ne-by-4 matrix
+%         
+%        Ne is the number of elements in the mesh. Each row in ele 
+%        contains the indices of the nodes for that mesh element.
+%   
+%   tnum: Label of phase, which corresponds to physical surface tag in Gmsh. 
+%         tnum is a Ne-by-1 array, where Ne is the number of elements.
+%         tnum(j,1) = k; means the j-th element belongs to the k-th phase.
+%         When omitted, assign one phase.
+%     
+%   ele_type: element type in Abaqus
+%             When omitted, ele_typ will be determined based on the size of 
+%             variable ele.
+%
+%   precision_nodecoor: number of digits to the right of the decimal point 
+%                       when writing node coordinates.
+%                       When omitted, precision_nodecoor=8;
+%
+%   path_file_name: file name of inp file, such as 'aaa.inp', 'D:\aaa.inp'.
+%                   When omitted, path_file_name='test.inp';
+%
+%
+% This is sub-project of Im2mesh package. If you use this function, please
+% cite as follows: 
+%  Ma, J., & Li, Y. (2025). Im2mesh: A MATLAB/Octave package for generating
+%  finite element mesh based on 2D multi-phase image (2.1.5). Zenodo. 
+%  https://doi.org/10.5281/zenodo.14847059
 %
 % Im2mesh is copyright (C) 2019-2025 by Jiexian Ma and is distributed under
 % the terms of the GNU General Public License (version 3).
@@ -20,7 +61,7 @@ function printInp2d( vert, tria, tnum, ele_type, precision_nodecoor, path_file_n
 %
 
 
-	% format of Inp file
+	% format of inp file
 	% ------------------------------------------------------------------------
 	% Heading
 	%
@@ -32,27 +73,78 @@ function printInp2d( vert, tria, tnum, ele_type, precision_nodecoor, path_file_n
     %
     % ------------------------------------------------------------------------
 
+    % ---------------------------------------------------------------------
+    % Check the number of inputs. If missing, set as empty. 
+    if nargin < 2 || nargin > 6
+        error("Wrong number of inputs");
+    end
+    
+    if nargin < 3
+        tnum = [];
+    end
+    
+    if nargin < 4
+        ele_type = [];
+    end
+    
+    if nargin < 5
+        precision_nodecoor = [];
+    end
+    
+    if nargin < 6
+        path_file_name = [];
+    end
+
+    % ---------------------------------------------------------------------
+    % check input size
+    if size(vert,2) >= 3
+        warning("Z coordnates of mesh node will be ignored.");
+        vert = vert( :, 1:2 );
+    end
+    
     % ------------------------------------------------------------------------
-    % check the number of inputs
-    if nargin == 5
+    % If input is empty, assign defaualt value to input
+    if isempty(tnum)
+        tnum = ones( size(ele,1), 1 );
+    end
+    
+    if isempty(ele_type)
+        ele_wid = size( ele, 2 );
+        
+        switch ele_wid
+            case 3
+                ele_type = 'CPS3';
+            case 6
+                ele_type = 'CPS6';
+            case 4
+                ele_type = 'CPS4';
+            case 8
+                ele_type = 'CPS8';
+            otherwise
+                error("Bad input in mesh elements.")
+        end
+    end
+    
+    if isempty(precision_nodecoor)
+        precision_nodecoor = 8;
+    end
+
+    if isempty(path_file_name)
         % write to current folder
         path_file_name = 'test.inp';
-    elseif nargin == 6
-        % write to the specified file directory, e.g. 'C:\test.inp'
-    else
-        error("check the number of inputs");
     end
     
     % ------------------------------------------------------------------------
     % Add node numbering and element numbering, and organize elements into 
     % cell array. eleC{i} represent elements in the i-th phase.
     
-    [ nodecoor, ~, eleC ] = getNodeEle( vert, tria, tnum );
-
+    [ nodecoor, ~, eleC ] = getNodeEle( vert, ele, tnum );
+    
     % ------------------------------------------------------------------------
     % convert number 1 2 3 to character A B C
     num_sect = length( eleC );
     sect_ascii = 65: ( 65 + num_sect - 1);
+    % section character
     sect_char = char( sect_ascii );     % 'ABCD...'
     
     % ------------------------------------------------------------------------
@@ -65,6 +157,8 @@ function printInp2d( vert, tria, tnum, ele_type, precision_nodecoor, path_file_n
     fmNodeNum = '%d';     % format_node_num
     fmEleNum = '%d';      % format_ele_num
     
+    % ---------------------------------------------------------------------
+    % start writing to file
     % ------------------------------------------------------------------------
 	fid=fopen(path_file_name,'wW');
     % ------------------------------------------------------------------------
@@ -72,6 +166,7 @@ function printInp2d( vert, tria, tnum, ele_type, precision_nodecoor, path_file_n
     fprintf( fid, [...
         '*Heading'                                              '\n'...
         '*Preprint, echo=NO, model=NO, history=NO, contact=NO'  '\n'...
+        '** INP file generated by Im2mesh package'              '\n'...
         '**'                                                    '\n'...
         ] ...
         );
@@ -141,7 +236,7 @@ function printEle( fid, ele, format_ele_num, format_node_num )
 % work for linear element and quadratic element
 
     num_column = size( ele, 2 );
-
+    
     % fprintf( fid, '%d,%d,%d,%d,%d\n', ele' );
     % example:
     % 3,173,400,475                 % linear tria element
