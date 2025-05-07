00f6fdc28c28c27e1b4d7bb2dd70183b58197079
Jason <mjx0799@gmail.com>
2025-05-06 21:29:43 -0400

    Update tria2BoundEdge.m

--- Im2mesh_Matlab/tria2BoundEdge.m
+++ Im2mesh_Matlab/tria2BoundEdge.m
@@ -17,7 +17,6 @@ function [edge, phaseEdge] = tria2BoundEdge( tria, tnum )
 %
 %
 % Copyright (C) 2019-2025 by Jiexian Ma, mjx0799@gmail.com
-% Distributed under the terms of the GNU General Public License (version 3)
 % 
 % Project website: https://github.com/mjx888/im2mesh
 %
