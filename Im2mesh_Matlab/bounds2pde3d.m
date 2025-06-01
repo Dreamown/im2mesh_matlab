b4fe0fd89b8cfa7e2f92d7890112e428d5a1963e
Jiexian Ma <mjx0799@gmail.com>
2025-05-31 23:48:07 -0400

    Update bounds2pde3d.m

--- Im2mesh_Matlab/bounds2pde3d.m
+++ Im2mesh_Matlab/bounds2pde3d.m
@@ -1,10 +1,48 @@
-function model3d = bounds2pde3d( bounds, height )
-% bounds2pde3d
+function model3d = bounds2pde3d( bounds, height, scale_factor )
+% bounds2pde3d: create Matlab 3d pde model object based on polygonal
+% boundaries.
+% What function bounds2pde3d does:
+%     create a 2d pde mode object
+%     extrude in the Z direction using 'height'
+%     add correct phase label
+%     scale node coordinates using 'scale_factor'
+%     create a 3d pde mode object
+%
+% input:
+%   bounds - A nested cell array of 2d polygonal boundaries.
+%            Polygons in bounds{i} belong to the i-th part or phase.
+%            bounds{i}{j} is one of the polygons in the i-th part.
+%            bounds{i}{j} is a n-by-2 array for x y coordinates of vertices
+%            in a polygon. You can use 
+%            plot( bounds{i}{j}(:,1), bounds{i}{j}(:,2) ) to view the
+%            polygon. Use plotBounds( bounds ) to view all polygons.
+%
+%   height - Extrusion height in the Z direction
+%
+%   scale_factor - scale factor for the node coordinites
+%
+% output:
+%   model3d - Matlab 3d pde model object
+%
+%
+% Copyright (C) 2019-2025 by Jiexian Ma, mjx0799@gmail.com
+% Distributed under the terms of the GNU General Public License (version 3)
+% 
+% Project website: https://github.com/mjx888/im2mesh
+%
 
+    % ---------------------------------------------------------------------
+    % create initial 2d mesh
+    
     [ poly_node, poly_edge ] = getPolyNodeEdge( bounds );
     [ node, edge, part ] = regroup( poly_node, poly_edge );
+    min_len = 1;
+    
     [vert,~,tria,tnum] = deltri1( node, edge, part );
     
+    % ---------------------------------------------------------------------
+    % create 2d pde model object and then extrude to create 3d model
+
     % Create matlab pde model object
     model_temp = createpde();  % qudratic model
     geometryFromMesh( model_temp, vert', tria', tnum' );
@@ -12,13 +50,34 @@ function model3d = bounds2pde3d( bounds, height )
     g = model_temp.Geometry;
     extrude( g, height );
     
-    generateMesh( model_temp, 'Hmax',500, 'Hmin', 1 );
+    generateMesh( model_temp, 'Hmax',500, 'Hmin', min_len );
     
+    % ---------------------------------------------------------------------
+    % add correct phase label to mesh and then scale node coordinates using
+    % scale_factor
+
     vert3d = model_temp.Mesh.Nodes';
     tria3d = model_temp.Mesh.Elements';
+
+    [vert3d,tria3d,tnum3d] = addPhaseLabel(vert3d,tria3d,node,edge,part);
+    vert3d = vert3d * scale_factor;
+
+    % Create matlab 3d pde model object
+    model3d = createpde();
     
-    
-    tnum3 = zeros(size(tria3d,+1),+1);
+    geometryFromMesh( model3d, vert3d', tria3d', tnum3d' );
+
+    model3d.Mesh = [];
+
+    % ---------------------------------------------------------------------
+end
+
+
+function [vert3d,tria3d,tnum3d] = addPhaseLabel(vert3d,tria3d,node,edge,part)
+% addPhaseLabel: add correct phase label (tnum3d) to the mesh (vert3d,tria3d)
+% according to 2d PSLG (node,edge,part)
+
+    tnum3d = zeros(size(tria3d,+1),+1);
     
     % tria midpoint
     tmid = vert3d(tria3d(:,1),:) ...
@@ -31,17 +90,10 @@ function model3d = bounds2pde3d( bounds, height )
     PSLG = edge;
     for ppos = 1: length(part)
        [stat] = inpoly2( tmid(:,1:2), node, PSLG(part{ppos},:) );
-       tnum3(stat)  = ppos;
+       tnum3d(stat)  = ppos;
     end
     
     % keep "interior" tria's
-    tria3d = tria3d(tnum3>+0,:);
-    tnum3 = tnum3(tnum3>+0,:);
-    
-    % Create matlab pde model object
-    model3d = createpde();
-    
-    geometryFromMesh( model3d, vert3d', tria3d', tnum3' );
-
-    model3d.Mesh = [];
+    tria3d = tria3d(tnum3d>+0,:);
+    tnum3d = tnum3d(tnum3d>+0,:);
 end
