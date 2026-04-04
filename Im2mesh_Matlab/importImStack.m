de166fef10e36027ae3bfdf43bb6311a5325b491
Jason <mjx0799@gmail.com>
2026-04-04 19:45:53 -0400

    Update importImStack.m

--- Im2mesh_Matlab/importImStack.m
+++ Im2mesh_Matlab/importImStack.m
@@ -5,7 +5,12 @@ function im = importImStack( file_name )
 % example: 
 %         file_name = 'test_stacked_image.tif';
 %         im = importImStack( file_name );
-%         
+%
+% Copyright (C) 2019-2026 by Jiexian Ma, mjx0799@gmail.com
+% Distributed under the terms of the GNU General Public License (version 3)
+% 
+% Project website: https://github.com/mjx888/im2mesh
+%
 
     info_image = imfinfo( file_name );
     num_file = length( info_image );
